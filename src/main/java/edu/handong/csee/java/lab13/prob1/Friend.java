49fc75e0b1fa382f60e87d22d91c13f89d7833c4
Isaac Chae <qpalzmm22@gmail.com>
2018-04-26 11:12:09 +0900

    Problem1 completed

--- src/main/java/edu/handong/csee/java/lab13/prob1/Friend.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/Friend.java
@@ -1,6 +1,5 @@
 package edu.handong.csee.java.lab13.prob1;
 
 public class Friend {
-	public Friend() {
-	}
+	
 }
--- src/main/java/edu/handong/csee/java/lab13/prob1/InstanceOf.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/InstanceOf.java
@@ -2,10 +2,26 @@ package edu.handong.csee.java.lab13.prob1;
 
 public class InstanceOf {
 
-	
+	public static void WhatFriend(Friend aFriend) {
+		if(aFriend instanceof ClassFriend) {
+			System.out.println("Class Friend!");
+		}
+		else if (aFriend instanceof SchoolFriend) {
+			System.out.println("School Friend!");
+		}
+		else {
+			System.out.println("Just a Friend!");
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Friend friend1 = new Friend();
+		Friend friend2 = new SchoolFriend();
+		Friend friend3 = new ClassFriend();
+		
+		InstanceOf.WhatFriend(friend1);
+		InstanceOf.WhatFriend(friend2);
+		InstanceOf.WhatFriend(friend3);
 	}
 
 }
